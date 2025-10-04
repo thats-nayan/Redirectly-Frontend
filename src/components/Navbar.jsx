fb4433acb2332ef488ba2b516c23db9606e6b90e
nayan <hanandynayan17@gmail.com>
2025-10-04 17:14:27 +0530

    Update navbar to reflect login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IoIosMenu } from "react-icons/io";
 import { RxCross2 } from "react-icons/rx";
-
+import { ContextApi } from "../contextApi/ContextApi";
+import { useContext } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { token , setToken} = useContext(ContextApi);
   const path = useLocation().pathname;
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   const onLogOutHandler = () => {
-    
+    setToken(null);
+    localStorage.removeItem("JWT_TOKEN");
+    navigate("/login");
   };
 
   return (
@@ -22,15 +26,13 @@ const Navbar = () => {
           </h1>
         </Link>
         <ul
-          className={`flex sm:gap-10 gap-4 sm:items-center sm:mt-1 sm:pt-0 pt-3 text-slate-800 sm:static absolute left-0 top-[62px] sm:shadow-none shadow-md ${
-            navbarOpen ? "h-fit sm:pb-0 pb-5" : "h-0 overflow-hidden"
-          }  transition-all duration-100 sm:h-fit sm:bg-none  bg-custom-gradient sm:w-fit w-full sm:flex-row flex-col px-4 sm:px-0`}
+          className={`flex sm:gap-10 gap-4 sm:items-center sm:mt-1 sm:pt-0 pt-3 text-slate-800 sm:static absolute left-0 top-[62px] sm:shadow-none shadow-md ${navbarOpen ? "h-fit sm:pb-0 pb-5" : "h-0 overflow-hidden"
+            }  transition-all duration-100 sm:h-fit sm:bg-none  bg-custom-gradient sm:w-fit w-full sm:flex-row flex-col px-4 sm:px-0`}
         >
           <li className="hover:text-btnColor font-[500]  transition-all duration-150">
             <Link
-              className={`${
-                path === "/" ? "text-white font-semibold" : "text-gray-200"
-              }`}
+              className={`${path === "/" ? "text-white font-semibold" : "text-gray-200"
+                }`}
               to="/"
             >
               Home
@@ -38,35 +40,41 @@ const Navbar = () => {
           </li>
           <li className="hover:text-btnColor font-[500]  transition-all duration-150">
             <Link
-              className={`${
-                path === "/about" ? "text-white font-semibold" : "text-gray-200"
-              }`}
+              className={`${path === "/about" ? "text-white font-semibold" : "text-gray-200"
+                }`}
               to="/about"
             >
               About
             </Link>
           </li>
-          <li className="hover:text-btnColor font-[500]  transition-all duration-150">
-            <Link
-              className={`${
-                path === "/dashboard" ? "text-white font-semibold" : "text-gray-200"
-              }`}
-              to="/dashboard"
-            >
-              Dashboard
-            </Link>
-          </li>
-          <Link to="/register">
+          {token && (
+            <li className="hover:text-btnColor font-[500]  transition-all duration-150">
+              <Link
+                className={`${path === "/dashboard" ? "text-white font-semibold" : "text-gray-200"
+                  }`}
+                to="/dashboard"
+              >
+                Dashboard
+              </Link>
+            </li>
+          )}
+
+          {!token && (
+            <Link to="/register">
               <li className=" sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
                 SignUp
               </li>
             </Link>
+          )}
 
-          <button
-             onClick={onLogOutHandler}
-             className="sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
+          {token && (
+            <button
+              onClick={onLogOutHandler}
+              className="sm:ml-0 -ml-1 bg-rose-700 text-white  cursor-pointer w-24 text-center font-semibold px-2 py-2 rounded-md  hover:text-slate-300   transition-all duration-150">
               LogOut
             </button>
+          )}
+          
         </ul>
         <button
           onClick={() => setNavbarOpen(!navbarOpen)}
